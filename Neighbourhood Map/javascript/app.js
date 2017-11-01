4303c9ef02f81e6cd96c3ff3797da77bc4c6e1ab
farisq1 <fbq_2007@hotmail.com>
2017-11-01 23:15:14 +0300

    Update app.js

--- Neighbourhood Map/javascript/app.js
+++ Neighbourhood Map/javascript/app.js
@@ -3,11 +3,11 @@
       // empty array for markers
       var markers = [];
       var locations = [
-          {title: 'Prince Sultan University', location: {lat: 24.734703, lng: 46.696186}},
-          {title: 'Hayatt Mall', location: {lat: 24.743317, lng: 46.680522}},
-          {title: 'Panorama Mall', location: {lat: 24.693029, lng: 46.669536}},
+          {title: 'Prince Sultan University', id:'52831ef911d24a6f3cb6eea5', location: {lat: 24.734703, lng: 46.696186}},
+          {title: 'Hayatt Mall',id:'4b73f92df964a5202ec22de3', location: {lat: 24.743317, lng: 46.680522}},
+          {title: 'Panorama Mall', id:'4bb60acd46d4a5932198c5c0', location: {lat: 24.693029, lng: 46.669536}},
           {title: 'Faisaliya Hotel',  id:'b8e8804f964a520332733e3', location: {lat: 24.690507, lng: 46.684123}},
-          {title: 'King Fahd International Stadium', location: {lat: 24.788540, lng: 46.839021}}
+          {title: 'King Fahd International Stadium', id:'518bcfa8498e8caf8a444ba6' , location: {lat: 24.788540, lng: 46.839021}}
           ];
       function initMap() {
         // Constructor creates a new map - only center and zoom are required.
@@ -22,18 +22,15 @@
         // The following group uses the location array to create an array of markers on initialize.
         for (var i = 0; i < locations.length; i++) {
           var position = locations[i].location;
-          var lat = locations[i].location.lat;
-          var lng = locations[i].location.lng;
+          var id = locations[i].id;
           var title = locations[i].title;
           // Create a marker per location, and put into markers array.
            var marker = new google.maps.Marker({
             map: map,
             position: position,
-            lat: lat,
-            lng, lng,
+            id: locations[i].id,
             title: title,
-            animation: google.maps.Animation.DROP,
-            id: i
+            animation: google.maps.Animation.DROP
             
           });
           // Push the marker to our array of markers.
@@ -59,30 +56,29 @@
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != marker) {
           infowindow.marker = marker;
-               
-              var id = locations.id;
-              var client_id = 'KHARCC5ZFU3J0C5E2ZKKWYXGYEYJ152G1TWQAIFSC5IJKXUB';
-              var client_secret = 'Y1RKB2UB4AODF0M3ONAH2MNK3KEJEEBTMRAK2NMHQ2WDGN0N';
-              var venueId = "457985253";
-              var forSquareUrl = 'https://api.foursquare.com/v2/venues/search?ll=' +id+'client_id='+client_id+'&client_secret='+client_secret;
-              console.log(forSquareUrl);
-          $.ajax( {
-              url:forSquareUrl ,
-              dataType: 'json'
-});           
-            function success(data) {
-             
-                $('.message').html('');
-                
-            }
+             var clientId = 'KHARCC5ZFU3J0C5E2ZKKWYXGYEYJ152G1TWQAIFSC5IJKXUB';
+  			var clientSecret = 'Y1RKB2UB4AODF0M3ONAH2MNK3KEJEEBTMRAK2NMHQ2WDGN0N';
+  			var version = '20172510';
+  			var forSquareUrl = 'https://api.foursquare.com/v2/venues/search?ll=' + marker.id + 'client_id=' + clientId + '&client_secret='+ clientSecret + '&v=' + version;
 
-            function fail() {
-              alert('Request failed');
-    }
 
+  	$.ajax({
+  		url:forSquareUrl
+
+  		}).done(function(data){
+  			console.log(data);
+  			var address = data.response.venue.location.address;
+  			var name = data.response.venue.name;
+			infoWindow.setContent('<h3>' + marker.title + '/' + name + '</h3>' + '<p>' + address + '</p>');
+  			infoWindow.open(map, marker);
+  			
+
+  		}).fail(function(){
+  			alert("Error failed to generate API");
+  		})
           
           
-          infowindow.setContent('<div>' + 'Name :' +  marker.title + '<br>' + ' Address : ' + marker.position + '</div>');
+          infowindow.setContent('<div>' + 'Name :' + marker.title + '<br>' + '</div>');
           infowindow.open(map, marker);
           // Make sure the marker property is cleared if the infowindow is closed.
           infowindow.addListener('closeclick', function() {
@@ -111,6 +107,7 @@
 
 
         if (locationTitle.includes(word)){
+            this.markers.google.maps.Animation.BOUNCE;
 
               return this.locations()[i];
         }
@@ -125,14 +122,13 @@
 
     };
   
-    }
-
-            
-  
-
- ko.applyBindings(new AppViewModel());
+    };
+   
 
 
+ 
+
+ko.applyBindings(new AppViewModel());
                               
  function openNav() {
     document.getElementById("mySidenav").style.display = "block";
@@ -141,3 +137,5 @@
 function closeNav() {
     document.getElementById("mySidenav").style.display = "none";
 }
+
+  
